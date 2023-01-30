c08a95dcf613c0b0faf66f7f5330ff8dc2680032
Artem <xenex95@yandex.ru>
2023-01-30 21:37:27 +0500

    Создана программа для решения первой задачи

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Укажите количество вводимых чисел: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int pos = 0;
+
+int PrintNums(int x, int pos)
+{
+    for (int i=1; i<=x; i++)
+    {
+        Console.Write($"Введите число {i}: ");
+        int y = Convert.ToInt16(Console.ReadLine());
+        if(y>0) 
+        {
+            pos+=1;
+        }
+    }
+
+    Console.WriteLine($"Количество введенных положительных чисел равно {pos}");
+    return pos;
+}
+
+PrintNums(x, pos);
